bc2bde8ea8eaf5e5ea2ba32b89b8f5a95a4cb058
akouz <akouz.mohamed999@gmail.com>
2017-06-01 15:51:50 +0000

    adding date picker to add RH

--- src/app/ajouter-Rh.component.ts
+++ src/app/ajouter-Rh.component.ts
@@ -4,6 +4,7 @@ import {FormGroup,FormBuilder,Validators,AbstractControl} from "@angular/forms";
 import {ProjetService} from './services/projet.service';
 import {RouterModule,Routes,Router,ActivatedRoute} from '@angular/router';
 import {FormValidator} from './validators/form-validator';
+import {IMyDpOptions} from 'mydatepicker';
 import {Location} from '@angular/common'
 import * as $ from 'jquery'
 
@@ -26,6 +27,10 @@ import * as $ from 'jquery'
     myEmail: AbstractControl;
     myPhoneNumber : AbstractControl;
     passwordError;
+    private myDatePickerOptions: IMyDpOptions = {
+        // other options...
+        dateFormat: 'dd/mm/yyyy',
+    };
     constructor(formBuilder:FormBuilder, private projetService: ProjetService,private route:ActivatedRoute,
     private router : Router,private _ngZone:NgZone,private _location:Location){
         this.form = formBuilder.group({
@@ -46,6 +51,16 @@ import * as $ from 'jquery'
         this.myPhoneNumber = this.form.controls['numeroTelephone'];
     }
 
+        dateToSql(date){
+            //sqlDate in SQL DATETIME format ("yyyy-mm-dd hh:mm:ss.ms")
+            var sqlDateArr1 = date.split("/");
+            //format of sqlDateArr1[] = ['yyyy','mm','dd hh:mm:ms']
+            var sYear = sqlDateArr1[2];
+            var sMonth = sqlDateArr1[1];
+            var sDay = sqlDateArr1[0];
+            var SqlDate=sYear+'-'+sMonth+'-'+sDay;
+            return SqlDate;
+        }
 
     onSubmit(dto){
         this.passwordError = null;
@@ -53,13 +68,16 @@ import * as $ from 'jquery'
             this.passwordError = "Les deux mot de passe sont pas équivalent !"
         }else{
 
-                 let collaborateur = {
+            let collaborateur = {
             nom:dto.nom,
             prenom:dto.prenom,
             adresse:dto.adresse,
             cin:dto.cin,
             password:dto.password,
-            email:dto.email
+            email:dto.email,
+            numeroTelephone:dto.numeroTelephone,
+            dateNaissance:this.dateToSql(dto.dateNaissance.formatted),
+            lieuNaissance:dto.lieuNaissance
         }
         if(dto.Type=="Administrateur"){
             this.projetService.addAdministrateur(collaborateur).subscribe(
